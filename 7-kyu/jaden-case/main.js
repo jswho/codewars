f150bf6687b4a3331ebfbc0701dd5bc07f7259fc
Oliver Arroyo <js_who@yahoo.com>
2022-02-26 16:52:28 +0000

    Created custom method to be added to String prototype - (Codewars wouldn't accept my functional version)

--- 7-kyu/jaden-case/main.js
+++ 7-kyu/jaden-case/main.js
@@ -8,11 +8,20 @@ Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 */
 
-const toJadenCase = (string) => {
+// const toJadenCase = (string) => {
+//   return string
+//     .split(' ')
+//     .map((word) => `${word[0].toUpperCase()}${word.substring(1)}`)
+//     .join(' ');
+// };
+
+String.prototype.toJadenCase = function () {
+  const string = Object.values(this).join('');
   return string
     .split(' ')
     .map((word) => `${word[0].toUpperCase()}${word.substring(1)}`)
     .join(' ');
 };
 
-module.exports = toJadenCase;
+// module.exports = toJadenCase;
+module.exports = String.prototype.toJadenCase;
--- 7-kyu/jaden-case/main.test.js
+++ 7-kyu/jaden-case/main.test.js
@@ -1,19 +1,22 @@
 const toJadenCase = require('./main');
 
 test('a single word, with the initial Jaden case spelling', () => {
-  expect(toJadenCase('How')).toBe('How');
+  const string = 'How';
+  expect(string.toJadenCase()).toBe('How');
 });
 
 test('an all lowercase word', () => {
-  expect(toJadenCase('jaden')).toBe('Jaden');
+  const string = 'jaden';
+  expect(string.toJadenCase()).toBe('Jaden');
 });
 
 test('two words at the beginning of a sentence', () => {
-  expect(toJadenCase('How can')).toBe('How Can');
+  const string = 'How can';
+  expect(string.toJadenCase()).toBe('How Can');
 });
 
 test('a full sentence', () => {
   const notJadenCase = "How can mirrors be real if our eyes aren't real";
   const jadenCase = "How Can Mirrors Be Real If Our Eyes Aren't Real";
-  expect(toJadenCase(notJadenCase)).toBe(jadenCase);
+  expect(notJadenCase.toJadenCase()).toBe(jadenCase);
 });
