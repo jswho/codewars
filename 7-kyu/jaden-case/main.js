4db4dbb6e2913d4fe13add6e01e6742faa103812
Oliver Arroyo <js_who@yahoo.com>
2022-02-26 15:38:43 +0000

    Test a single word, with the initial Jaden case spelling.

--- 7-kyu/jaden-case/main.js
+++ 7-kyu/jaden-case/main.js
@@ -8,6 +8,8 @@ Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 */
 
-const toJadenCase = (string) => {};
+const toJadenCase = (string) => {
+  return string;
+};
 
 module.exports = toJadenCase;
--- 7-kyu/jaden-case/main.test.js
+++ 7-kyu/jaden-case/main.test.js
@@ -1,1 +1,5 @@
 const toJadenCase = require('./main');
+
+test('a single word, with the initial Jaden case spelling', () => {
+  expect(toJadenCase('How')).toBe('How');
+});
