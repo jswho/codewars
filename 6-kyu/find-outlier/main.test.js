ed42a4a76d70db8bdfba06c0a364ff73a8a7925e
Oliver Arroyo <js_who@yahoo.com>
2022-03-01 23:35:01 +0000

    Test for multiple integers.

--- 6-kyu/find-outlier/main.test.js
+++ 6-kyu/find-outlier/main.test.js
@@ -7,3 +7,7 @@ test('at least 3 integers and returns the outlier', () => {
 test('at least 3 integers and returns an odd outlier', () => {
   expect(findOutlier([16, 17, 18])).toBe(17);
 });
+
+test('multiple integers', () => {
+  expect(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36])).toBe(11);
+});
