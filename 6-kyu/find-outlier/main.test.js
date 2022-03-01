4f278650cafb73024e226af8493c8a9d127ec2f7
Oliver Arroyo <js_who@yahoo.com>
2022-03-01 23:37:14 +0000

    Test for multiple integers with a negative integer included.

--- 6-kyu/find-outlier/main.test.js
+++ 6-kyu/find-outlier/main.test.js
@@ -11,3 +11,7 @@ test('at least 3 integers and returns an odd outlier', () => {
 test('multiple integers', () => {
   expect(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36])).toBe(11);
 });
+
+test('multiple integers with a negative integer included', () => {
+  expect(findOutlier([160, 3, 1719, 19, 11, 13, -21])).toBe(160);
+});
