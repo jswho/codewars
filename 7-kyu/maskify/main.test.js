76e01f6a1e28a73a7e3d036bc6de9084bbeb4c83
Oliver Arroyo <js_who@yahoo.com>
2022-03-01 21:23:35 +0000

    Test for a long string.

--- 7-kyu/maskify/main.test.js
+++ 7-kyu/maskify/main.test.js
@@ -8,3 +8,7 @@ test('a string less than or equal to four characters in length, should return it
 test('a string that is 5 characters in length, should have its first character masked', () => {
   expect(maskify('12345')).toBe('#2345');
 });
+
+test('a long string that has last 4 characters unmasked', () => {
+  expect(maskify('4556364607935616')).toBe('############5616');
+});
