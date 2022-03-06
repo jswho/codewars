ec981de42a027040ebd33d49dd0d5ac5d8be40f8
Oliver Arroyo <js_who@yahoo.com>
2022-03-06 13:29:30 +0000

    Input on tests is now enclosed in an array.

--- 7-kyu/people-on-bus/main.test.js
+++ 7-kyu/people-on-bus/main.test.js
@@ -1,5 +1,14 @@
 const number = require('./main');
 
 test('for a single bus stop, with 0 people coming on and off the bus', () => {
-  expect(number([0, 0])).toBe(0);
+  expect(number([[0, 0]])).toBe(0);
+});
+
+test('for two bus stops, with 0 people coming on and 1 off', () => {
+  expect(
+    number([
+      [3, 0],
+      [0, 1],
+    ])
+  ).toBe(2);
 });
