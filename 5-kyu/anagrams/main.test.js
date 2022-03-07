f62c1ee0d2b08fa7633593bcd07a5f5f61c2071f
Oliver Arroyo <js_who@yahoo.com>
2022-03-07 09:40:25 +0000

    Test for multiple words.

--- 5-kyu/anagrams/main.test.js
+++ 5-kyu/anagrams/main.test.js
@@ -7,3 +7,10 @@ test('a word that is not an anagram', () => {
 test('a word that is an anagram', () => {
   expect(anagrams('racer', ['carer'])).toStrictEqual(['carer']);
 });
+
+test('multiple words', () => {
+  expect(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada'])).toStrictEqual([
+    'aabb',
+    'bbaa',
+  ]);
+});
