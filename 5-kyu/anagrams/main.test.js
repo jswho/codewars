529e9d23d4eb4b7ef0424c4094a30d6d833b9dbb
Oliver Arroyo <js_who@yahoo.com>
2022-03-07 09:38:53 +0000

    Test for a word that is an anagram.

--- 5-kyu/anagrams/main.test.js
+++ 5-kyu/anagrams/main.test.js
@@ -3,3 +3,7 @@ const anagrams = require('./main');
 test('a word that is not an anagram', () => {
   expect(anagrams('laser', ['lazy'])).toStrictEqual([]);
 });
+
+test('a word that is an anagram', () => {
+  expect(anagrams('racer', ['carer'])).toStrictEqual(['carer']);
+});
